b15f6ca53a5d9f97f45b536c82c472b59126bbb2
Eugene1547 <135571286+Eugene1547@users.noreply.github.com>
2025-01-25 15:39:27 -0500

    Login&registration

---
+++ iFridge/iFridge/LoginView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  iFridge
+//
+//  Created by qunliu on 1/22/25.
+//
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseCore
+
+class FirebaseManager: NSObject {
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        
+        super.init()
+    }
+}
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    init() {
+        
+    }
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")){
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    Button{
+                        
+                    }label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(.all, 12.0)
+                    .background(Color(.secondaryLabel).opacity(0.12))
+                    .cornerRadius(16)
+                    
+                    
+                    Button{
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Login" :"Create Accoutn")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 12)
+                            
+                            
+                            Spacer()
+                        }
+                        .background(Color.black)
+                        .cornerRadius(32)
+                    }
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.black)
+                }
+                .padding(32)
+            }
+            .navigationTitle(isLoginMode ? "Login" : "Create Account")
+        }
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("should log into Firebase with existing credentials")
+            loginUser()
+            
+        } else {
+            createNewAccount()
+//            print ("should create a new user account in Firebase")
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Error logging in:", err)
+                self.loginStatusMessage = "Failed to login user"
+                return
+            }
+            
+            print("Sucessfully logged in: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Login successful"
+        }
+    }
+    
+    @State var loginStatusMessage = " "
+    
+    private func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Error creating user:", err)
+                self.loginStatusMessage = err.localizedDescription
+                return
+            }
+            
+            print("Sucessfully created a new user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "User created successfully"
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ iFridge/iFridgeTests/iFridgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  iFridgeTests.swift
+//  iFridgeTests
+//
+//  Created by 刘群 on 1/22/25.
+//
+
+import Testing
+
+struct iFridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
