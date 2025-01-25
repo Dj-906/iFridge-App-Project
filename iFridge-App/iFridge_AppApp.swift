9f7075d402f65e35c57bb3297440feff9548489d
Dj-906 <dennisjr412@gmail.com>
2025-01-25 14:47:32 -0500

    Initial Commit

---
+++ iFridge-App/iFridge_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFridge_AppApp.swift
+//  iFridge-App
+//
+//  Created by Dennis Austin Jr on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct iFridge_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iFridge-AppTests/iFridge_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iFridge_AppTests.swift
+//  iFridge-AppTests
+//
+//  Created by Dennis Austin Jr on 1/25/25.
+//
+
+import Testing
+@testable import iFridge_App
+
+struct iFridge_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
