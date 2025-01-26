78f20d8d8a31ed80fd6c115967a11f6902262214
Eugene1547 <135571286+Eugene1547@users.noreply.github.com>
2025-01-26 14:06:07 -0500

    Profile page with upload image function; firebase storage; put the firebase configure to main app file, now login view and profile view will crash in preview but the application is able to run

---
+++ iFridge/Models/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  iFridge
+//
+//  Created by 刘群 on 1/26/25.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
